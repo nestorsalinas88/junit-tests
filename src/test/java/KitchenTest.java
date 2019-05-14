0f095d27252c2ad5877ecdfabf7a7bd22efd0837
Fernando Mendoza R <fernandoza@gmail.com>
2019-05-14 12:17:26 -0500

    refactor: clean up on diff instances of the object

--- src/test/java/KitchenTest.java
+++ src/test/java/KitchenTest.java
@@ -1,39 +1,41 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.*;
 
 public class KitchenTest {
 
+    private Kitchen emptyKitchen;
+    private Kitchen kitchenWithOne;
+    private Kitchen kitchenWithMany;
+
+    @Before
+    public void setUp(){
+        emptyKitchen = new Kitchen();
+        kitchenWithMany = new Kitchen();
+        kitchenWithOne = new Kitchen();
+
+        kitchenWithOne.add("white corn");
+
+        kitchenWithMany.add("blue corn");
+        kitchenWithMany.add("yellow corn");
+        kitchenWithMany.add("yellow corn");
+        kitchenWithMany.add("white corn");
+    }
+
     @Test
     public void testIsEmpty(){
-        Kitchen emptyKitchen = new Kitchen();
         assertTrue(emptyKitchen.isEmpty());
-
-        Kitchen kitchen = new Kitchen();
-        kitchen.add("yellow corn");
-
-        assertFalse(kitchen.isEmpty());
+        assertFalse(kitchenWithOne.isEmpty());
     }
 
 
     @Test
     public void testSize(){
-        Kitchen emptyKitchen = new Kitchen();
         assertSame(0, emptyKitchen.tortillas.length);
-
-        Kitchen kitchenWithOne = new Kitchen();
-        kitchenWithOne.add("yellow corn");
         assertSame(1, kitchenWithOne.tortillas.length);
-
         // Testing sizes is tricky set boundaries is always a good idea
         // test 0, 1 and many (!2)
-
-        Kitchen kitchenWithMany = new Kitchen();
-        kitchenWithMany.add("blue corn");
-        kitchenWithMany.add("yellow corn");
-        kitchenWithMany.add("yellow corn");
-        kitchenWithMany.add("white corn");
-
         assertSame(4, kitchenWithMany.tortillas.length);
     }
 }
