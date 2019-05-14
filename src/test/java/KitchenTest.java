193bc66bea5b0ec8b20cb7f174bd52548c21e447
Fernando Mendoza R <fernandoza@gmail.com>
2019-05-14 12:11:00 -0500

    test: finish test for size with 0, 1 and many (!2)

--- src/test/java/KitchenTest.java
+++ src/test/java/KitchenTest.java
@@ -1,7 +1,6 @@
 import org.junit.Test;
 
-import static junit.framework.TestCase.assertFalse;
-import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.*;
 
 public class KitchenTest {
 
@@ -14,5 +13,27 @@ public class KitchenTest {
         kitchen.add("yellow corn");
 
         assertFalse(kitchen.isEmpty());
+    }
+
+
+    @Test
+    public void testSize(){
+        Kitchen emptyKitchen = new Kitchen();
+        assertSame(0, emptyKitchen.tortillas.length);
+
+        Kitchen kitchenWithOne = new Kitchen();
+        kitchenWithOne.add("yellow corn");
+        assertSame(1, kitchenWithOne.tortillas.length);
+
+        // Testing sizes is tricky set boundaries is always a good idea
+        // test 0, 1 and many (!2)
+
+        Kitchen kitchenWithMany = new Kitchen();
+        kitchenWithMany.add("blue corn");
+        kitchenWithMany.add("yellow corn");
+        kitchenWithMany.add("yellow corn");
+        kitchenWithMany.add("white corn");
+
+        assertSame(4, kitchenWithMany.tortillas.length);
     }
 }
