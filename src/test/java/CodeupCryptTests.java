77e1fc726d1429836b0e14949d374795184b35df
Fernando Mendoza <fernandoza@gmail.com>
2019-08-22 14:10:40 -0500

    feat: create test file and method for password and hash

--- src/test/java/CodeupCryptTests.java
+++ src/test/java/CodeupCryptTests.java
@@ -1,10 +1,13 @@
 package java;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class CodeupCryptTests {
     @Test
     public void testHasPasswordAndHash(){
         CodeupCrypt crypt = new CodeupCrypt();
+        assertNotEquals(crypt.getPassword, "");
+        assertNotEquals(crypt.getHash, "");
     }
 }
