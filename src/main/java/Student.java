7ea500712439445b54aa03daf0f339f238e5fbda
nestor salinas <nestorasm88@gmail.com>
2019-08-26 15:47:39 -0500

    Chore: setting up J-Unit

---
+++ src/main/java/Student.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+
+    String name;
+    Long property_id;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public Student(Long property_id) {
+        this.property_id = property_id;
+    }
+
+//    public Grades() {
+//        this.students = new ArrayList<>();
+//    }
+//    name and property_id,
+//   and initializes the grades property as an empty ArrayList.
+//
+    // returns the student's id
+    public long getId() {
+        return 0;
+    }
+
+    // returns the student's name
+    public String getName() {
+        return null;
+    }
+
+    // adds the given grade to the grades property
+    public void addGrade(int grade) {
+
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage() {
+        return 0;
+    }
+
+}
